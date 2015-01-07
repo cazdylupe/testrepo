de28d9eb8e34aea60a52d765b48da60c0e3e291c
Raj Narayanan <cazdylupe@gmail.com>
2015-01-07 23:35:14 +0530

    Modified server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/*
+ * Testing Node request Module
+ */
 var request = require('request');
 request('http://www.google.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
