f85023c24b74bc141453a2e21f7b94897a4a7dda
Raj Narayanan <cazdylupe@gmail.com>
2015-01-08 14:21:58 +0530

    APplied vaiable name conventions

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
  * Testing Node request Module.
  * @Author Raj Narayanan
  */
-var request = require('request');
-request('http://www.google.com', function (error, response, body) {
+var _request = require('request');
+
+_request('http://www.google.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body) // Print the google web page.
   }
