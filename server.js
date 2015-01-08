cd71210c404149c0f391934970af6f588ab5438e
Raj Narayanan <cazdylupe@gmail.com>
2015-01-08 14:21:02 +0530

    Docs for request..

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
  * @Author Raj Narayanan
  */
 var request = require('request');
+
+/**
+ * Add documentation here
+ */
 request('http://www.google.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body) // Print the google web page.
